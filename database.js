0f826ca27314f17de005a832adb6b94763419d89
sriharan40 <sriharan40@gmail.com>
2017-04-02 15:25:59 +0530

    Update database.js

--- database.js
+++ database.js
@@ -168,7 +168,7 @@ var update_params = {
 };
 
 console.log("Updating the item...");
-docClient.update(params, function(err, data) {
+docClient.update(update_params, function(err, data) {
     if (err) {
         console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
     } else {
